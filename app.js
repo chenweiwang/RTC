19b3dbe5b86c2a7e705c80ec2f4edff2ac4d9c7c
JackWangCS <jackwangcs@outlook.com>
2015-11-09 22:05:29 +0800

    refresh project

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 var newRequest = require('./newRequest.js'),
-    authenticate = require('./authenticate.js');
+    authenticate = require('./authenticate.js'),
+    Updater = require('./Updater.js');
 
 var rootUrl = "https://opentechtest.chinacloudapp.cn:9443/jazz";
 var username = "jack";
 var password = "jack";
 var request = newRequest();
+var updater = new Updater(request, rootUrl);
 
-if (authenticate(request, rootUrl, username, password)) {
-    console.log("Login failed!");
-} else {
-    console.log("Login to " + rootUrl + " success!");
-}
+authenticate(request, rootUrl, username, password, function(err) {
+    if (err) {
+        console.log("Login failed!");
+    } else {
+        console.log("Login to " + rootUrl + " success!");
+        console.log("Begin to refresh resource..");
+        updater.getProjects(function(err, projects) {
+            if (err) {
+                console.log("Fetch projects failed!");
+            } else {
+                console.log(projects);
+            }
+        })
+    }
+
+});
+
 
 
 
--- authenticate.js
+++ authenticate.js
@@ -4,29 +4,30 @@
 
 
 var authPath = "/authenticated/j_security_check";
-var authenticate = function (request, rootUrl, username, password) {
+var authenticate = function (request, rootUrl, username, password, callback) {
     request.get(rootUrl, function(err) {
        if (err) {
            console.log('Error: ' + err.message);
-           return 'Can not connect to Sever';
+           return callback('Can not connect to Sever');
        } else {
            var authUrl = rootUrl + authPath;
            request.post({url: authUrl, form: {j_username: username, j_password: password}}, function(err, res) {
                if (err) {
                    console.log('Auth Error: ' + err.message);
-                   return 'Auth Error' + err.message;
+                   return callback('Auth Error' + err.message);
                } else {
                     var authMsg = res.headers['X-com-ibm-team-repository-web-auth-msg'];
                     //Auth failed!
                     if (authMsg && (authMsg === 'authrequired' || isLogined === 'authfailed')) {
-                        return 'AuthFailed';
+                        return callback('AuthFailed');
                     } else {
-                        return null;
+                        return callback(null);
                     }
                }
            });
        }
     });
+
 };
 
 module.exports = authenticate;
