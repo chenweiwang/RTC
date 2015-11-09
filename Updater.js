525b7aecb878c33c19913699e7ca563d365c16d0
JackWangCS <jackwangcs@outlook.com>
2015-11-09 22:04:27 +0800

    Add Updater.js

---
+++ Updater.js
@@ -0,0 +1,40 @@
+/**
+ * Created by jack8 on 2015/11/9.
+ */
+
+function Updater(request, rootUrl) {
+    this.rootUrl = rootUrl;
+    this.request = request;
+    this.rootServicesUrl = rootUrl + "/rootservices";
+}
+
+module.exports = Updater;
+
+//get the projects
+Updater.prototype.getProjects = function(callback) {
+    var request = this.request;
+    var options = {
+        url: this.rootServicesUrl,
+        headers: {
+            'Accept': 'application/json'
+        }
+    };
+    request.get(options, function(err, res) {
+        if (err) {
+            return callback(err);
+        }
+        var rootServices = JSON.parse(res.body);
+        var projectsUrl = rootServices["https://opentechtest.chinacloudapp.cn:9443/jazz/rootservices"]
+            ["http://open-services.net/xmlns/cm/1.0/cmServiceProviders"][0].value;
+        console.log("Projects Url: " + projectsUrl);
+        options.url = projectsUrl;
+        options.headers.Accept = '*/*';
+        request.get(projectsUrl, function(err, res) {
+            if (err) {
+                return callback(err);
+            }
+            return callback(null, res.body);
+        });
+    });
+}
+
