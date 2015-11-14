f992700ca9aa71052fcdc9831b081df6b301377b
JackWangCS <jackwangcs@outlook.com>
2015-11-14 20:56:11 +0800

    add xml parse example

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var newRequest = require('./newRequest.js'),
     authenticate = require('./authenticate.js'),
-    Updater = require('./Updater.js');
+    Updater = require('./Updater.js'),
+    parseString = require('xml2js').parseString;
 
 var rootUrl = "https://opentechtest.chinacloudapp.cn:9443/jazz";
 var username = "jack";
@@ -14,15 +15,24 @@ authenticate(request, rootUrl, username, password, function(err) {
     } else {
         console.log("Login to " + rootUrl + " success!");
         console.log("Begin to refresh resource..");
-        updater.getProjects(function(err, projects) {
+        updater.getProjects(function(err, projectsXml) {
             if (err) {
                 console.log("Fetch projects failed!");
             } else {
-                console.log(projects);
+                console.log(projectsXml);
+                parseString(projectsXml, function(err, projects) {
+                    if (err) {
+                        console.log("Parse xml Error");
+                    } else {
+                        //console.log(projects);
+                        var tmp = projects["oslc_disc:ServiceProviderCatalog"]["oslc_disc:entry"][0];
+                        var result = tmp["oslc_disc:ServiceProvider"][0]["oslc_disc:services"];
+                        console.log(result[0]["$"]["rdf:resource"]);
+                    }
+                })
             }
         })
     }
-
 });
 
 
